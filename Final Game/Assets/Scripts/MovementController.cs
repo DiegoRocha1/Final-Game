04c55b309a7426a4d6dd1210889c9b7384e4b957
Diego Rocha <diegorocha@email.arizona.edu>
2018-11-27 16:19:54 -0700

    Metadata

---
+++ Final Game/Assets/Scripts/MovementController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+   
+	public bool isGrounded;
+	public float vertSpeed;
+	public float horzSpeed;
+	
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void OnCollisionStay() {
+		isGrounded = true;
+	}
+	
+	void Update() {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * horzSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * vertSpeed;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+		
+		//if (Input.GetKeyDown ("space")){
+		//	transform.Translate(Vector3.up * Time.deltaTime * 30, Space.World);
+		//}	
+	}
+}
--- Final Game/Assets/Scripts/PlayController.cs
+++ Final Game/Assets/Scripts/PlayController.cs
@@ -7,42 +7,51 @@ public class PlayController : MonoBehaviour
 
 	public GameObject hand;
 	public GameObject balloon;
+	public AnimationClip clip;
+	private Animation anim;
+	
 	public float speed;
+	
 	// Use this for initialization
 	void Start () {
-		
+		anim = hand.GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			//while (hand.transform.rotation.x < 50)
-			//{
-				
-			//}
-			//hand.transform.position = new Vector3(hand.transform.position.x, 5, hand.transform.position.z);
-			//while (hand.transform.eulerAngles.x < 50f)
-			//{
-			//	float deg = hand.transform.eulerAngles.x;
-			//	hand.transform.eulerAngles = new Vector3(deg+1, hand.transform.eulerAngles.y, hand.transform.eulerAngles.z);
-				//hand.transform.Rotate(Vector3.right, speed * Time.deltaTime);
-			//}
+			anim.clip = clip;
+			
+			anim.Play();
 		}
+		//(Input.GetKeyDown(KeyCode.Space))
+//		{
+//			//while (hand.transform.rotation.x < 50)
+//			//{
+//				
+//			//}
+//			//hand.transform.position = new Vector3(hand.transform.position.x, 5, hand.transform.position.z);
+//			//while (hand.transform.eulerAngles.x < 50f)
+//			//{
+//			//	float deg = hand.transform.eulerAngles.x;
+//			//	hand.transform.eulerAngles = new Vector3(deg+1, hand.transform.eulerAngles.y, hand.transform.eulerAngles.z);
+//				//hand.transform.Rotate(Vector3.right, speed * Time.deltaTime);
+//			//}
+//		}
 	}
 
 	void OnCollisionEnter(Collision c)
 	{
-		float force = 200;
-		if (c.gameObject == balloon)
-		{
-			Vector3 dir = c.contacts[0].point - transform.position;
-			// We then get the opposite (-Vector3) and normalize it
-			dir = dir.normalized;
-			// And finally we add force in the direction of dir and multiply it by force. 
-			// This will push back the player
-			GetComponent<Rigidbody>().AddForce(dir*force);
-		}
+//		float force = 200;
+//		if (c.gameObject == balloon)
+//		{
+//			Vector3 dir = c.contacts[0].point - transform.position;
+//			// We then get the opposite (-Vector3) and normalize it
+//			dir = dir.normalized;
+//			// And finally we add force in the direction of dir and multiply it by force. 
+//			// This will push back the player
+//			GetComponent<Rigidbody>().AddForce(dir*force);
+//		}
 	}
 }
