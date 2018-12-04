f46e286ec87e5cd2a5f5a79460632b74a3ac742b
Diego Rocha <diegorocha@email.arizona.edu>
2018-12-04 00:29:39 -0700

    Added force to balloon on collision

---
+++ Final Game/Assets/Scripts/BalloonHandContact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonHandContact : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody rb;
+	public GameObject hand;
+	public GameObject balloon;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+
+	void Update()
+	{
+//		Debug.Log(hand.transform.position);
+//		Debug.Log(balloon.transform.position);
+		
+		if (Mathf.Abs(hand.transform.position[1] - balloon.transform.position[1]) <= 1.5 && 
+		    (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)))
+		{
+			rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+		}
+
+	}
+//	private void OnCollisionEnter(Collision other)
+//	{
+//		rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+//	}
+}
